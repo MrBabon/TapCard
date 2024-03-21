36c1cf673ceec36e4a8e26fa65e9c07d23e263f2
MrBabon <christophedanna06@gmail.com>
2024-03-21 10:51:42 +0100

    suppression table follow

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_15_081008) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_21_092648) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -162,16 +162,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_15_081008) do
     t.datetime "updated_at", null: false
     t.index ["entreprise_id"], name: "index_exhibitors_on_entreprise_id"
     t.index ["event_id"], name: "index_exhibitors_on_event_id"
-  end
-
-  create_table "follows", force: :cascade do |t|
-    t.integer "following_id", null: false
-    t.integer "follower_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["follower_id"], name: "index_follows_on_follower_id"
-    t.index ["following_id", "follower_id"], name: "index_follows_on_following_id_and_follower_id", unique: true
-    t.index ["following_id"], name: "index_follows_on_following_id"
   end
 
   create_table "messages", force: :cascade do |t|
